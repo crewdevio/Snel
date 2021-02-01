8f2b894acab0a33e36ea6cc69d9cbd296457cb87
Erick Sosa <ericksosagarcias@gmail.com>
2021-02-01 17:23:56 -0400

    feat: build in router lib

--- compiler/types.ts
+++ compiler/types.ts
@@ -103,4 +103,5 @@ export interface BuildOptions {
   dev?: boolean;
   dist?: boolean;
   fileOutPut?: string;
+  generate?: "dom" | "ssr"
 }
---
+++ core/router/mod.js
@@ -0,0 +1,5 @@
+export { default as Router } from "./Router.js";
+export { default as Route } from "./Route.js";
+export { default as Link } from "./Link.js";
+export { link, links } from "./actions.js";
+export { navigate } from "./history.js";
