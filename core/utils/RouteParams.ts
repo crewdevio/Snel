2a9ae75be2ddf00e32a1c0940a585509e672a456
Erick Sosa <ericksosagarcias@gmail.com>
2021-05-05 01:29:36 -0400

    feat(core/utils): NormalizePathName function

--- core/utils/RouteParams.ts
+++ core/utils/RouteParams.ts
@@ -8,6 +8,10 @@
 
 import { match } from "./imports/path_to_regexp.ts";
 
+/**
+ * Match route from path name.
+ */
+
 export function MatchRoute(path: string, route: string) {
   const mtc = match(path, { decode: decodeURIComponent });
 
@@ -18,4 +22,20 @@ export function GetParams(path: string, route: string) {
   const mtc = match(path, { decode: decodeURIComponent });
 
   return mtc(route)!;
+}
+
+/**
+ * Normalize a pathname for matching, replaces multiple slashes with a single
+ * slash and normalizes unicode characters to "NFC". When using this method,
+ * `decode` should be an identity function so you don't decode strings twice.
+ */
+export function NormalizePathName(path: string) {
+  return (
+    decodeURI(path)
+      // Replaces repeated slashes in the URL.
+      .replace(/\/+/g, "/")
+      // Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/normalize
+      // Note: Missing native IE support, may want to skip this step.
+      .normalize()
+  );
 }
