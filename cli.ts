a6893aa62103689c15c991da9ad374b8fece8376
Gil Rudolf Härdi <47613233+ghaerdi@users.noreply.github.com>
2021-05-31 12:19:41 -0400

    refactor: move commands conditionals into an object (#18)
    Co-authored-by: ghaerdi <ghaerdi@intellisys.com.do>

--- cli.ts
+++ cli.ts
@@ -6,9 +6,9 @@
  *
  */
 
-import { showHelp, common, resolverConfigFile } from "./src/shared/utils.ts";
-import { HelpCommand, CommandNotFound } from "./src/shared/log.ts";
-import { PromptConfig, notFoundConfig } from "./src/cli/prompt.ts";
+import { common, resolverConfigFile, showHelp } from "./src/shared/utils.ts";
+import { CommandNotFound, HelpCommand } from "./src/shared/log.ts";
+import { notFoundConfig, PromptConfig } from "./src/cli/prompt.ts";
 import { VERSION as svelteVersion } from "./compiler/compiler.ts";
 import { VERSION as cliVersion } from "./src/shared/version.ts";
 import { flags, keyWords } from "./src/shared/utils.ts";
@@ -18,96 +18,100 @@ import { RollupBuild } from "./compiler/build.ts";
 import Build from "./src/cli/commands/build.ts";
 import { colors } from "./imports/fmt.ts";
 
+const { args: Args } = Deno;
+type Command = "create" | "serve" | "dev" | "build";
+const command = Args[0] as Command;
+
+const instructs = {
+  // create a template project
+  async create() {
+    if (flags.help.includes(Args[1])) {
+      return HelpCommand({
+        command: {
+          alias: [keyWords.create],
+          description: "create a template project",
+        },
+        flags: [{ alias: flags.help, description: "show command help" }],
+      });
+    }
+
+    else await CreateProject({ ...PromptConfig(), projectName: Args[1] });
+  },
+  // compile an bundle for production
+  async build() {
+    if (flags.help.includes(Args[1])) {
+      return HelpCommand({
+        command: {
+          alias: [keyWords.build],
+          description: "build application for production",
+        },
+        flags: [{ alias: flags.help, description: "show command help" }],
+      });
+    }
+
+    else if (await resolverConfigFile()) await Build();
+
+    else notFoundConfig();
+  },
+  // compile in dev mode
+  async dev() {
+    if (flags.help.includes(Args[1])) {
+      return HelpCommand({
+        command: {
+          alias: [keyWords.build],
+          description: "build application in dev mode",
+        },
+        flags: [{ alias: flags.help, description: "show command help" }],
+      });
+    }
+
+    else if (await resolverConfigFile()) {
+      console.time(colors.green("Compiled successfully in"));
+      await RollupBuild({ dir: common.dom.dir, entryFile: common.entryFile });
+      console.timeEnd(colors.green("Compiled successfully in"));
+    }
+
+    else notFoundConfig();
+  },
+  // start dev server
+  async serve() {
+    if (flags.help.includes(Args[1])) {
+      return HelpCommand({
+        command: {
+          alias: [keyWords.serve],
+          description: "build and server in a dev server",
+        },
+        flags: [{ alias: flags.help, description: "show command help" }],
+      });
+    }
+
+    else if (await resolverConfigFile()) await StartDev();
+
+    else notFoundConfig();
+  },
+};
+
 async function Main() {
-  const { args } = Deno;
-
   try {
-    // create a template project
-    if (args[0] === keyWords.create) {
-      if (flags.help.includes(args[1])) {
-        return HelpCommand({
-          command: {
-            alias: [keyWords.create],
-            description: "create a template project",
-          },
-          flags: [{ alias: flags.help, description: "show command help" }],
-        });
-      }
-
-      else {
-        await CreateProject({ ...PromptConfig(), projectName: Deno.args[1] });
-      }
+    // execute instructions
+    if (instructs[command]) {
+      await instructs[command as Command]();
     }
-    // compile an bundle for production
-    else if (args[0] === keyWords.build) {
-      if (flags.help.includes(args[1])) {
-        return HelpCommand({
-          command: {
-            alias: [keyWords.build],
-            description: "build application for production",
-          },
-          flags: [{ alias: flags.help, description: "show command help" }],
-        });
-      }
 
-      else if (await resolverConfigFile()) await Build();
-
-      else notFoundConfig();
-    }
-    // compile in dev mode
-    else if (args[0] === keyWords.dev) {
-      if (flags.help.includes(args[1])) {
-        return HelpCommand({
-          command: {
-            alias: [keyWords.build],
-            description: "build application in dev mode",
-          },
-          flags: [{ alias: flags.help, description: "show command help" }],
-        });
-      }
-
-      else if (await resolverConfigFile()) {
-        console.time(colors.green("Compiled successfully in"));
-        await RollupBuild({ dir: common.dom.dir, entryFile: common.entryFile });
-        console.timeEnd(colors.green("Compiled successfully in"));
-      }
-
-      else {
-        notFoundConfig();
-      }
-    }
-    // start dev server
-    else if (args[0] === keyWords.serve) {
-      if (flags.help.includes(args[1])) {
-        return HelpCommand({
-          command: {
-            alias: [keyWords.serve],
-            description: "build and server in a dev server",
-          },
-          flags: [{ alias: flags.help, description: "show command help" }],
-        });
-      }
-
-      else if (await resolverConfigFile()) {
-        await StartDev();
-      }
-
-      else {
-        notFoundConfig();
-      }
-    }
     // show version
-    else if (flags.version.includes(args[0])) {
+    if (flags.version.includes(Args[0])) {
       console.log(
         colors.green(
-          `snel: ${colors.yellow(cliVersion)}\nsvelte: ${colors.yellow(
-            svelteVersion
-          )}\ndeno: ${colors.yellow(Deno.version.deno)}`
-        )
+          `snel: ${colors.yellow(cliVersion)}\nsvelte: ${
+            colors.yellow(
+              svelteVersion,
+            )
+          }\ndeno: ${colors.yellow(Deno.version.deno)}`,
+        ),
       );
     }
     // show help
-    else if (flags.help.includes(args[0])) {
+    else if (flags.help.includes(Args[0])) {
       showHelp();
     }
 
