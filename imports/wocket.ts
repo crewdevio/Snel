9e16c371a89a21ec30b35704fed73f8306d70f82
buttercubz <ericksosagarcias@gmail.com>
2021-08-03 20:26:36 -0400

    fix: connection close error #30

--- imports/wocket.ts
+++ imports/wocket.ts
@@ -1,1 +1,1 @@
-export * from "https://denopkg.com/crewdevio/wocket@main/mod.ts";
+export * from "https://denopkg.com/crewdevio/wocket@dev/mod.ts";
--- install.ts
+++ install.ts
@@ -17,7 +17,7 @@ import "https://cdn.skypack.dev/svelte@3.38.2/store";
 const install = async (name: string, url: string, importmap?: boolean) => {
   const process = Deno.run({
     cmd: [
-      ...`${Deno.execPath()} install -A -f -r --no-check${
+      ...`${Deno.execPath()} install -q -A -f -r --no-check${
         importmap
           ? ` --import-map=https://deno.land/x/trex/import_map.json`
           : ""
